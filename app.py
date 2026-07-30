8ada94f424f61277b4517b9f3b23e10ad497dfe7
aroraayush296-cmyk <aroraayush296@gmail.com>
2026-07-30 11:01:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ profile = st.sidebar.multiselect(
 
 #========Get user input=======
 st.markdown("""### Get user info""")
-user_info=st.txt_area("""Write your resume description:"""")
+user_info=st.txt_area("""Write Your Resume Description: """)
 
 
 model = ChatGoogleGenerativeAI(
