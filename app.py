f7fb1af627bb8911ea9f635764d7738392d54d7f
aroraayush296-cmyk <aroraayush296@gmail.com>
2026-07-30 11:06:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -178,3 +178,21 @@ def get_jobs(agent,
 
 #code = get_jobs(agent)
 #DISPLAY.HTML(code)
+
+if st.button("Generate Resume"):
+    with st.spinner("Agent Running"):
+        code = main_agent(agent, user_info)
+        st.html(
+            code,
+            width="stretch",
+            unsafe_allow_javascript=True
+        )
+
+        st.divider()  # to give horizontal div
+
+        jobe_code = get_jobs(agent, location, profile)
+        st.html(
+            job_code,
+            width="stretch",
+            unsafe_allow_javascript=True
+        )
